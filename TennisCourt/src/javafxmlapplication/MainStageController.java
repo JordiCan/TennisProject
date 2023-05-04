3b68f47339d5a8d40061e949e599c3c0bf54b2fa
AlmaBravo2 <almabrapal@gmail.com>
2023-05-04 16:08:36 +0200

    Mi ramita

--- TennisCourt/src/javafxmlapplication/MainStageController.java
+++ TennisCourt/src/javafxmlapplication/MainStageController.java
@@ -4,16 +4,30 @@
  */
 package javafxmlapplication;
 
+import java.awt.Image;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.MenuItem;
+import javax.swing.JPanel;
 
 /**
  * FXML Controller class
  *
  * @author Alma
  */
-public class MainStageController implements Initializable {
+public class MainStageController extends javax.swing.JFrame implements Initializable {
+
+    @FXML
+    private MenuItem signUpScene;
+    @FXML
+    private MenuItem registerScene;
+    @FXML
+    private MenuItem fieldsScene;
+    
+    FondoPanel fondo = new FondoPanel();
 
     /**
      * Initializes the controller class.
@@ -21,6 +35,22 @@ public class MainStageController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+        this.setContentPane(fondo);
     }    
+
+    @FXML
+    private void changeToSignUp(ActionEvent event) {
+    }
+
+    @FXML
+    private void changeToRegister(ActionEvent event) {
+    }
+
+    @FXML
+    private void changeToFields(ActionEvent event) {
+    }
+ 
+    
+    
     
 }
