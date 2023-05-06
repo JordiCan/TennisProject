b65b7dcb120512873fc1586ef59c9a83a92cb950
Ana <napirosi@gmail.com>
2023-05-06 12:50:59 +0200

    profileView
    Parte del scene builder, sin campos a rellenar.

---
+++ TennisCourt/src/javafxmlapplication/ProfileController.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package javafxmlapplication;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import model.Club;
+import model.ClubDAOException;
+
+/**
+ * FXML Controller class
+ *
+ * @author ana
+ */
+public class ProfileController implements Initializable {
+    
+    public static Stage Main;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+        
+       // Image image = new Image ("/img/chaval.png");
+       // chaval.setImage(image);
+        
+    }    
+    
+}
